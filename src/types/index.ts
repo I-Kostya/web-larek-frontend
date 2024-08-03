ddc67d579152227476e3846f08f3f8cd96f8eb11
Kostya Ischuk <slubrusher@yandex.ru>
2024-08-04 00:23:10 +0700

    added documentation & base components

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,53 +1,60 @@
 export interface IProduct {
-	_id: string;
-	description: string;
-	image: string;
-	title: string;
-	category: TCategory;
-	price: number | null;
+  _id: string;
+  title: string;
+  description: string;
+  category: TCategory;
+  image: string;
+  price: number | null;
+}
+
+export interface IProductsList {
+  products: IProduct[];
 }
 
 export interface IAppState {
-	catalog: IProduct[];
-	preview: string | null;
-}
-
-// модель корзины
-export interface IBasket {
-	items: IProduct[];
-  addProduct: (product: IProduct) => void;
-  deleteProduct: (productId: string, payload: Function | null) => void;
-}
-
-
-// модель данных
-
-export interface IAppModel {
+  catalog: IProduct[];
+  basket: string[];
+  preview: string | null;
   order: IOrder;
-  
+  total: string | number;
+  loading: boolean;
 }
 
-export interface IOrder {
-	payment: string;
-	address: string;
-	email: string;
-	phone: string;
+export interface IOrderForm {
+  payment: string;
+  address: string;
+  email: string;
+  phone: string;
+}
+
+export interface IOrder extends IOrderForm {
+  items: string[];
 }
 
 export interface IOrderResult {
-	id: string;
-	total: number;
+  id: string;
+  total: number;
 }
 
+
 export type TCategory =
-	| 'софт-скил'
-	| 'другое'
-	| 'дополнительное'
-	| 'кнопка'
-	| 'хард-скил';
+  | 'софт-скил'
+  | 'другое'
+  | 'дополнительное'
+  | 'кнопка'
+  | 'хард-скил';
 
-export type TPaymentMethod = 'онлайн' | '' | 'при получении';
+export type TBasketItem = Pick<IProduct, 'title' | 'price'>;
 
-export type TPaymentForm = Pick<IOrder, 'payment' | 'address'>;
+export type TPreviewItem = Pick<
+  IProduct,
+  'title' | 'image' | 'description' | 'price'
+>;
 
-export type TContactsForm = Pick<IOrder, 'email' | 'phone'>;
+export type TPaymentMethod = 'онлайн' | 'при получении';
+
+export type TPaymentForm = Pick<IOrderForm, 'payment' | 'address'>;
+
+export type TContactsForm = Pick<IOrderForm, 'email' | 'phone'>;
+
+export type FormErrors = Partial<Record<keyof IOrderForm, string>>;
