fb3975fbbfb33aeba64340ab4c37a8d9b2d82f98
Kostya Ischuk <slubrusher@yandex.ru>
2024-08-17 05:52:33 +0700

    remove unused testData

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './scss/styles.scss';
 import { AppState } from './components/AppData';
 import { EventEmitter } from './components/base/events';
-import { API_URL, testData } from './utils/constants';
+import { API_URL } from './utils/constants';
 import { LarekApi } from './components/LarekApi';
 import { Page } from './components/Page';
 import { Card, CardBasket, CardPreview } from './components/Card';
@@ -36,15 +36,6 @@ const contacts = new Сontacts(cloneTemplate(contactsTemplate), events);
 events.onAll((event) => {
   console.log(event.eventName, event.data);
 });
-
-api
-  .getProductList()
-  .then((data) => {
-    appData.setCatalog(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
 
 events.on('catalog:changed', () => {
   page.catalog = appData.catalog.map((item) => {
@@ -133,65 +124,76 @@ events.on('order:open', () => {
       payment: '',
       address: '',
       valid: false,
-      errors: []
-    })
+      errors: [],
+    }),
   });
 });
 
 events.on('payment:changed', (item: HTMLButtonElement) => {
   appData.order.payment = item.name;
-})
+});
 
 events.on('formErrors:change', (errors: Partial<IOrderForm>) => {
   const { email, phone, address, payment } = errors;
   order.valid = !address && !payment;
   contacts.valid = !email && !phone;
-  order.errors = Object.values({address, payment}).filter(i => !!i).join('; ');
-  contacts.errors = Object.values({phone, email}).filter(i => !!i).join('; ');
+  order.errors = Object.values({ address, payment })
+    .filter((i) => !!i)
+    .join('; ');
+  contacts.errors = Object.values({ phone, email })
+    .filter((i) => !!i)
+    .join('; ');
 });
 
-events.on(/^contacts\..*:change/, (data: { field: keyof IOrderForm, value: string }) => {
-  appData.setContactsField(data.field, data.value);
-});
+events.on(
+  /^contacts\..*:change/,
+  (data: { field: keyof IOrderForm; value: string }) => {
+    appData.setContactsField(data.field, data.value);
+  }
+);
 
 // Изменилось одно из полей заказа - сохраняем данные об этом
-events.on(/^order\..*:change/, (data: { field: keyof IOrderForm, value: string }) => {
-  appData.setOrderField(data.field, data.value);
-});
+events.on(
+  /^order\..*:change/,
+  (data: { field: keyof IOrderForm; value: string }) => {
+    appData.setOrderField(data.field, data.value);
+  }
+);
 
 events.on('order:submit', () => {
-  appData.order.total = appData.getTotal()
+  appData.order.total = appData.getTotal();
   modal.render({
     content: contacts.render({
       email: '',
       phone: '',
       valid: false,
-      errors: []
-    })
+      errors: [],
+    }),
   });
-})
+});
 
 events.on('contacts:submit', () => {
-  api.orderProducts(appData.order)
+  api
+    .orderProducts(appData.order)
     .then(() => {
-      console.log(appData.order)
+      console.log(appData.order);
       const success = new Success(cloneTemplate(successTemplate), {
         onClick: () => {
           modal.close();
           appData.clearBasket();
           page.counter = appData.basket.length;
-        }
+        },
       });
-    
+
       modal.render({
         content: success.render({
-          total: appData.getTotal()
-        })
-      })
+          total: appData.getTotal(),
+        }),
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
-    })
+    });
 });
 
 events.on('modal:open', () => {
@@ -201,3 +203,12 @@ events.on('modal:open', () => {
 events.on('modal:close', () => {
   page.locked = false;
 });
+
+api
+  .getProductList()
+  .then((data) => {
+    appData.setCatalog(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
